e390c6e1e4f1d8569bada6a6c3ec63853ed43be6
afzal <afzal.swe@gmail.com>
2022-09-27 00:02:24 -0700

    Delete This Navbar, Create a new File and update navbar in this component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
 import React from 'react'
-import Nav from './components/navbar/Nav'
-
-
+import Nav from './Component/navbar/navbar'
+import Home from './Component/home/home'
+import About from './Component/about/about_idab'
+import Member from './Component/members/members'
+import Constitution from './Component/constitution/constitution'
+import Event from './Component/events/events'
+import Notice from './Component/notice/notice'
+import Publication from './Component/publication/publication'
+import Contact from './Component/contact/contact'
 
 const App = () => {
   return (
     <div>
-      <Nav />
+        <Nav />
+        <Home />
+        <About/>
+        <Member />
+        <Constitution />
+        <Event />
+        <Notice />
+        <Publication />
+        <Contact />
+      
 
     </div>
   )
 }
 
 export default App
+
 
---
+++ src/Component/about/about_idab.js
@@ -0,0 +1,73 @@
+// import React, { useState } from "react";
+import React from 'react'
+class about_idab extends React.Component {
+    render() {
+        return (
+            <h1>Welcome To About Section Page!!</h1>
+        );
+    }
+}
+
+export default about_idab;
+
+// class about_idab extends React.Component {
+//     render() {
+//         return(
+//             <div>
+//                 <section class="contact-area pt-80 pb-80">
+//                     <div class="container">
+//                         <div class="row">
+//                             <div class="col-xl12 mb-60">
+//                                 <div class="section-title text-center">
+//                                     <h4>contact us</h4>
+//                                     <p>You Can Contact Me !!</p>
+//                                 </div>
+//                             </div>
+//                         </div>
+//                         <div class="row">
+//                             <div class="col-xl7">
+//                                 <div class="contact-form">
+//                                     <form action="">
+//                                         <input type="text" placeholder="Name">
+//                                         <input type="text" placeholder="Subject">
+//                                         <input type="email" placeholder="Email">
+//                                         <textarea placeholder="Message"></textarea>
+//                                         <input type="submit" value="send">
+//                                     </form>
+//                                 </div>
+//                             </div>
+//                         </div>
+//                     <div class="col-xl5">
+//                         <div class="contact-address">
+//                             <div class="single-contact">
+//                                 <i class="fa-solid fa-location-dot"></i>
+//                                 <h4>Address <span>Banasree Block-B, Road-5, House-21, Dhaka</span></h4>
+//                             </div>
+//                             <div class="single-contact">
+//                                 <i class="fa-solid fa-square-phone"></i>
+//                                 <h4>Phone<span>+880 1811178307<br/>+880 1611178307</span></h4>
+//                             </div>
+//                             <div class="single-contact">
+//                                 <i class="fa-solid fa-envelope"></i>
+//                                 <h4>Email<span>contact@afzal.com</span></h4>
+//                             </div>
+//                             <div class="single-contact">
+//                                 <i class="fa-solid fa-earth-asia"></i>
+//                                 <h4>website<span>https://www.linkedin.com/in/afzal-swe/</span></h4>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </div>
+//                 <!-- <div class="button">
+//                     <center>
+//                         <span class="credit">Created By <a href="https://engrafzalhossen.com/afzal-swe">Engr Afzal Hossen</a> | @ 2022 all right reserved.</span>
+//                     </center> -->
+//                     </div>
+//             </div>
+//         </section>
+//         </div>
+//         );
+//     }
+// }
+
+// export default about_idab;
