0a14393cf6a62d7298164687a5d917a1c5809cc4
afzal <afzal.swe@gmail.com>
2022-09-28 23:07:21 -0700

    some changed contact section

--- src/Component/contact/contact.js
+++ src/Component/contact/contact.js
@@ -1,12 +1,18 @@
 import React from 'react'
-class contact extends React.Component {
-    render() {
-        return (
-            <h1>Welcome To Contact Section Page!!</h1>
-        );
-    }
+
+const Contact = () => {
+    return(
+        <div>
+            <section class="contact-area pt-80 pb-80">
+                <div class="container">
+
+                </div>
+            </section>
+        </div>
+
+    );
 }
-export default contact;
+export default Contact;
 
 
 
