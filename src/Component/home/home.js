6f50800bd637c38908964aff5906ace07c4c6498
unknown <you@example.com>
2022-10-10 16:21:22 +0600

    Banner Removed

--- src/Component/home/home.js
+++ src/Component/home/home.js
@@ -1,11 +1,8 @@
 import React from 'react'
-import Banner from '../banner/Banner';
 class home extends React.Component {
     render() {
         return (
-            <div className="container">
-                
-                <Banner /> 
+            <div className="container"> 
     
             </div>
         );
